40079f956ef6196145b72541edea22ccee39964e
Vaibhav <itsvaibhav002@gmail.com>
2025-01-17 12:54:53 +0530

    update

---
+++ linklist/single.java
@@ -0,0 +1,25 @@
+package linklist;
+
+class Node{
+    int data;
+    Node next;
+    Node(int data1,Node next1){
+        this.data=data1;
+        this.next=next1;
+    }
+    Node(int data1){
+        this.data=data1;
+        this.next=null;
+    }
+
+};
+
+
+public class single {
+    public static void main(String[] args) {
+        int arr[]={3,5,6,3};
+        Node y=new Node(arr[0]);
+        System.out.println(y.data);
+    }
+    
+}
